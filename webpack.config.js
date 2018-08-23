3cd2897bb02d05c3a229a3e174761ce8ebad7084
Pang Yaran <PangYaran@hotmail.sg>
2018-08-23 22:26:44 +0800

    Fix bugs webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,12 @@ var config = {
             }
         ]
     },
-    plugins: [
+    plugins: process.env.NODE_ENV === 'production' ?  [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
         new webpack.optimize.UglifyJsPlugin()
-    ]
+    ] : []
 };
 
 module.exports = config;
