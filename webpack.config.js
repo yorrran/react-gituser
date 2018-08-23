6a8c041292159793c37f382ff8f418912f759b74
Pang Yaran <PangYaran@hotmail.sg>
2018-08-23 22:19:26 +0800

    Add webpack production

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ var config = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
+        new webpack.optimize.UglifyJsPlugin()
     ]
 };
 
